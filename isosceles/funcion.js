4fb465da3d4b88d9e84f79412f6008f022423aeb
Emilio <emilioplatzer@gmail.com>
2016-08-24 16:04:06 -0300

    Pruebas para isósceles

---
+++ isosceles/funcion.js
@@ -0,0 +1,5 @@
+function isosceles(a,b,c){
+    return a==b && b==c;
+}
+
+module.exports = isosceles;
---
+++ isosceles/probar.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var mostrarTodos = true;
+
+var funcionAProbar = require('./funcion.js')
+
+var fs=require('fs');
+
+var txtPrueba = fs.readFileSync('pruebas.txt',{encoding:'utf8'});
+
+var lineas = txtPrueba.split(/\r?\n/);
+
+var cantidadPruebas = 0;
+var cantidadErrores = 0;
+
+lineas.forEach(function(linea){
+    var datos = linea.split(';');
+    if(linea.trim() && datos.length){
+        cantidadPruebas++;
+        var parametros = datos.slice(1);
+        if(parametros.length != funcionAProbar.length){
+            console.log("ERROR en el archivo de casos de prueba se esperaban",funcionAProbar.length,'parámetros y hay',parametros.length,'en la línea',linea)
+            return false;
+        }
+        var obtenido = funcionAProbar.apply(null, parametros);
+        var esperado = datos[0]=='si';
+        if(obtenido != esperado){
+            cantidadErrores++;
+            console.log('Error en',parametros,'se obtuvo',obtenido,'y se esperaba',esperado);
+        }else{
+            if(mostrarTodos){
+                console.log('Ok en',linea);
+            }
+        }
+    }
+});
+
+console.log('Cantidad de pruebas',cantidadPruebas)
+console.log('Cantidad de errores',cantidadErrores)
