d1c53be4a16bef8d5b2a63452df23f21cec465d9
Emilio <emilioplatzer@gmail.com>
2016-08-24 14:47:32 -0300

    detecto casos con distinta cantidad de parametros

--- palindromo/probar.js
+++ palindromo/probar.js
@@ -14,16 +14,23 @@ var cantidadPruebas = 0;
 var cantidadErrores = 0;
 
 lineas.forEach(function(linea){
-    cantidadPruebas++;
-    var datos = linea.split('=')
-    var obtenido = funcionAProbar.apply(null, datos[1].split(';'));
-    var esperado = datos[0]=='si';
-    if(obtenido != esperado){
-        cantidadErrores++;
-        console.log('Error en',datos[1],'se obtuvo',obtenido,'y se esperaba',esperado);
-    }else{
-        if(mostrarTodos){
-            console.log('Ok en',linea);
+    var datos = linea.split(';');
+    if(linea.trim() && datos.length){
+        cantidadPruebas++;
+        var parametros = datos.slice(1);
+        if(parametros.length != funcionAProbar.length){
+            console.log("ERROR en el archivo de casos de prueba se esperaban",funcionAProbar.length,'parámetros y hay',parametros.length,'en la línea',linea)
+            return false;
+        }
+        var obtenido = funcionAProbar.apply(null, parametros);
+        var esperado = datos[0]=='si';
+        if(obtenido != esperado){
+            cantidadErrores++;
+            console.log('Error en',datos[1],'se obtuvo',obtenido,'y se esperaba',esperado);
+        }else{
+            if(mostrarTodos){
+                console.log('Ok en',linea);
+            }
         }
     }
 });
